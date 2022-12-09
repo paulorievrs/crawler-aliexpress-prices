2e7840504b4bdcbc8b58c42c4545d5dfb81de523
paulorievrs <paulorievrsoliveira@gmail.com>
2022-12-08 21:29:08 -0300

    adding cron

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { CrawlerService } from './Services/CrawlerService/Crawler.service';
 require('dotenv').config();
 
-const crawlerService = new CrawlerService();
+import cron from 'node-cron';
 
-(async () => {
-  await crawlerService.crawl();
-})();
+cron.schedule('0 1 * * *', () => {
+  const crawlerService = new CrawlerService();
+
+  (async () => {
+    await crawlerService.crawl();
+  })();
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
+
